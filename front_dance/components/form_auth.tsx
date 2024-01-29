0bd0336585833c22aa1a47e84c8f7e2c0ca713e3
Sergey Cheh <chehstream@gmail.com>
2024-01-29 16:05:14 +0000

    add:make form relative for mobile and desktop

--- front_dance/components/form_auth.tsx
+++ front_dance/components/form_auth.tsx
@@ -1,19 +1,25 @@
-export default function LoginForm(){
+export default function LoginForm() {
     return (
         <div className="flex h-screen justify-center bg-gradient-to-t from-fuchsia-900 to-red-500">
-            <div className="mt-20 w-2/6">
+            <div className="mt-20 sm:w-3/6 lg:w-2/6">
                 <h1 className="flex justify-center text-2xl text-red-900 font-bold mb-2 mt-2 ">dancesport.am</h1>
                 <div className="bg-white rounded-2xl w-full">
                     <p className="flex justify-center pt-10 text-xl font-bold">Войти в профиль</p>
                     <form action="">
-                        <input type="text" />
-                        <input type="text" />
-                        <br />
-                        <a href="/#">Не помните пароль?</a>
-                        <br />
-                        <button>Войти</button>
-                        <br />
-                        <a href="/#">Создать аккаунт</a>
+                        <div className="flex-col flex justify-center w-full p-6">
+                            <div className="my-2 flex">
+                                <input className="bg-red-400 px-4 h-12 rounded-lg w-full" type="text" />
+                            </div>
+                            <div className="my-2 flex">
+                                <input className="bg-red-400 px-4 h-12 rounded-lg w-full" type="password" />
+                            </div>
+                            <br />
+                            <a className="underline" href="/#">Не помните пароль?</a>
+                            <br />
+                            <button className="w-full h-12 bg-blue-400 rounded-lg">Войти</button>
+                            <br />
+                            <a className="w-full h-12 border-2 rounded-lg flex justify-center items-center" href="/#">Создать аккаунт</a>
+                        </div>
                     </form>
                 </div>
             </div>
