918ff72f87a032ad9ac411adaa9b9b77c3101e6f
ChehST <chehstream@gmail.com>
2024-03-31 21:58:26 +0400

    test: add acceptence test

--- front_dance/app/AppAccessability.cy.tsx
+++ front_dance/app/AppAccessability.cy.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import Home from './page'
 
-describe('<Home />', () => {
-  it('renders', () => {
+describe('App successfully starts', () => {
+  it('Home page renders', () => {
     // see: https://on.cypress.io/mounting-react
     cy.mount(<Home />)
   })
